997bee95b508cd563d5972f89807fb98c23bc8c8
bsvalverde <bsvalverde@gmail.com>
2021-03-07 18:27:58 -0300

    Creates tests and updates CandidateService

--- backend/src/services/CandidateService.ts
+++ backend/src/services/CandidateService.ts
@@ -1,4 +1,5 @@
-import Candidate, { CandidateStore } from '../types/candidates';
+import Candidate, { CandidateQuery, CandidateStore } from '../types/candidates';
+import Experience from '../types/experience';
 
 interface CandidateServiceConstructor {
   store: CandidateStore;
@@ -11,7 +12,36 @@ export default class CandidateService {
     this.store = store;
   }
 
-  async list(): Promise<Candidate[]> {
-    return this.store.list();
+  getExperienceFilter(
+    minExperience?: number,
+    maxExperience?: number,
+  ): Experience[] {
+    const experience = Object.values(Experience);
+
+    let minExperienceIndex = 0;
+    if (minExperience && minExperience > 0) {
+      minExperienceIndex = Math.min(minExperience, experience.length - 1);
+    }
+    let maxExperienceIndex;
+    if (maxExperience) {
+      maxExperienceIndex =
+        minExperienceIndex === maxExperience
+          ? minExperienceIndex + 1
+          : maxExperience;
+    }
+
+    return experience.slice(minExperienceIndex, maxExperienceIndex);
+  }
+
+  async list({
+    limit,
+    maxExperience,
+    minExperience,
+  }: CandidateQuery): Promise<Candidate[]> {
+    let experience;
+    if (maxExperience || minExperience) {
+      experience = this.getExperienceFilter(minExperience, maxExperience);
+    }
+    return this.store.list({ experience, limit });
   }
 }
