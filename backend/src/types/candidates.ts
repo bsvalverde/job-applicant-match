2e590033137e62d5b1709f3986b109d1e5d1dee3
bsvalverde <bsvalverde@gmail.com>
2021-03-07 17:53:55 -0300

    Refactors type files

--- backend/src/types/candidates.ts
+++ backend/src/types/candidates.ts
@@ -1,18 +1,4 @@
-enum Experience {
-  zeroToOne = '0-1 years',
-  oneToTwo = '1-2 years',
-  twoToThree = '2-3 years',
-  threeToFour = '3-4 years',
-  fourToFive = '4-5 years',
-  fiveToSix = '5-6 years',
-  sixToSeven = '6-7 years',
-  sevenToEight = '7-8 years',
-  eightToNine = '8-9 years',
-  nineToTen = '9-10 years',
-  tenToEleven = '10-11 years',
-  elevenToTwelve = '11-12 years',
-  twelvePlus = '12+ years',
-}
+import Experience from './experience';
 
 interface Technology {
   name: string;
@@ -26,6 +12,17 @@ export default interface Candidate {
   technologies: Technology[];
 }
 
+export interface CandidateQuery {
+  limit?: number;
+  minExperience?: number;
+  maxExperience?: number;
+}
+
+export interface CandidateFilter {
+  experience?: Experience[];
+  limit?: number;
+}
+
 export interface CandidateStore {
-  list: () => Promise<Candidate[]>;
+  list: (filter: CandidateFilter) => Promise<Candidate[]>;
 }
