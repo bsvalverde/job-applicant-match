8418c702f58be134652b364b05c3832dc5bfab7a
bsvalverde <bsvalverde@gmail.com>
2021-03-07 22:49:26 -0300

    Adds providers to RootContainer

--- frontend/src/containers/RootContainer.tsx
+++ frontend/src/containers/RootContainer.tsx
@@ -1,26 +1,28 @@
-// import Box from '@material-ui/core/Box';
-// import ThemeProvider from '@material-ui/styles/ThemeProvider';
+import Box from '@material-ui/core/Box';
+import ThemeProvider from '@material-ui/styles/ThemeProvider';
 import React from 'react';
-// import { IntlProvider } from 'react-intl';
-// import { BrowserRouter } from 'react-router-dom';
-// import { getIntlProps } from '../intl';
-// import theme from '../theme';
+import { IntlProvider } from 'react-intl';
+import { BrowserRouter } from 'react-router-dom';
+import { getIntlProps } from '../intl';
+import theme from '../theme';
 // import AppContainer from './AppContainer';
 
 const RootContainer = () => {
-  // const intlProps = getIntlProps();
+  const intlProps = getIntlProps();
 
   return (
-    // <IntlProvider {...intlProps}>
-    //   <ThemeProvider theme={theme}>
-    //     <BrowserRouter>
-    //       <Box height="100%" width="100%">
-            // <AppContainer />
-            <p>test</p>
-    //       </Box>
-    //     </BrowserRouter>
-    //   </ThemeProvider>
-    // </IntlProvider>
+    <IntlProvider {...intlProps}>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Box height="100%" width="100%">
+            {
+              // <AppContainer />
+              <p>test</p>
+            }
+          </Box>
+        </BrowserRouter>
+      </ThemeProvider>
+    </IntlProvider>
   );
 };
 
