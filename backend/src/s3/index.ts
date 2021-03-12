cfa6bdb1e436cb9bc74f253b3e87069aed11a4f4
bsvalverde <bsvalverde@gmail.com>
2021-03-11 23:25:39 -0300

    Removes unused store

--- backend/src/s3/index.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const { S3_URL = 'https://geekhunter-recruiting.s3.amazonaws.com/code_challenge.json' } = process.env;
-
-const s3 = axios.create({
-  baseURL: S3_URL,
-});
-
-export default s3;
