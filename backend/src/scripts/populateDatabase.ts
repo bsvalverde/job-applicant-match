3cc125870c13f1e62e80699c17304fced1d4d653
bsvalverde <bsvalverde@gmail.com>
2021-03-13 17:43:54 -0300

    Adds job data to database population script

--- backend/src/scripts/populateDatabase.ts
+++ backend/src/scripts/populateDatabase.ts
@@ -1,11 +1,13 @@
 import { camelizeKeys } from 'humps';
 import data from '../dummyData';
 import MongoCandidate from '../mongo/models/Candidate';
+import MongoJob from '../mongo/models/Job';
 
 export default async () => {
   await MongoCandidate.deleteMany({});
+  await MongoJob.deleteMany({});
 
-  const { candidates } = data;
+  const { candidates, jobs } = data;
 
   candidates.forEach(({ id, city, experience, technologies }) => {
     MongoCandidate.create({
@@ -13,6 +15,17 @@ export default async () => {
       city,
       experience: parseInt(experience.split('-')[0]),
       technologies: technologies.map((technology) => camelizeKeys(technology)),
+    });
+  });
+
+  jobs.forEach(({ id, city, technologies, experience }) => {
+    const [minExperience, maxExperience] = experience.split(' ')[0].split('-');
+    MongoJob.create({
+      id,
+      city,
+      minExperience,
+      maxExperience,
+      technologies,
     });
   });
 };
